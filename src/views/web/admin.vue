58247adb409de7ab69c8cab1c9fd7051efe0b579
test <test@qq.com>
2022-04-08 20:34:39 +0800

    整合后台管理系统登录

--- src/views/web/admin.vue
+++ src/views/web/admin.vue
@@ -5,6 +5,7 @@
 export default {
   name: 'admin',
   mounted () {
+    // 后台管理系统登录路径
     const devUrl = 'http://localhost:9528/login'
     window.open(devUrl, '_blank')
     // _blank - URL加载到一个新的窗口。这是默认值
